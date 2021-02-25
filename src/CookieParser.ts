ee159f5363aa24b48d8be84b3bbae595f5c07357
Azul <diego.kfu@hotmail.com>
2021-02-25 17:51:50 -0300

    v3.0.4

--- src/CookieParser.ts
+++ src/CookieParser.ts
@@ -2,7 +2,9 @@
 import { CookieJar } from 'tough-cookie';
 
 const Parse = (Objects: any, cookieJar: CookieJar) => {
-  for (let i = 0, keys = Object.keys(Objects); i < keys.length; i += 1) {
+  let keys: string[] | any = Object.keys(Objects);
+
+  for (let i = 0; i < keys.length; i += 1) {
     const CookieObject = Objects[keys[i]];
 
     const now = new Date(CookieObject.creation);
@@ -12,16 +14,19 @@ const Parse = (Objects: any, cookieJar: CookieJar) => {
 
     cookieJar.setCookieSync(`${CookieObject.key}=${CookieObject.value}`, CookieObject.domain, o);
   }
+
+  keys = null;
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function CookieParser(RequestJar: any): CookieJar {
   const cookieJar = new CookieJar();
 
-  for (let i = 0, keys = Object.keys(RequestJar); i < keys.length; i += 1) {
+  let keys: string[] | any = Object.keys(RequestJar);
+  for (let i = 0; i < keys.length; i += 1) {
     Parse(RequestJar[keys[i]]['/'], cookieJar);
   }
-
+  keys = null;
   return cookieJar;
 }
 
