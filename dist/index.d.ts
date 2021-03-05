188d09ee90e8b3acd50dbcd082e72d6134f4cd1d
Azul <diego.kfu@hotmail.com>
2021-03-05 03:18:27 -0300

    v3.0.5

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -7,7 +7,7 @@ export interface Config {
     enableGC?: boolean;
 }
 declare const _default: {
-    Loader: (SteamID64: string, appID: string | number, contextID: string | number, Config: Config) => Promise<AzulInventoryResponse>;
+    Loader: (SteamID64: string, appID: string | number, contextID: string | number, LoaderConfig: Config) => Promise<AzulInventoryResponse>;
     getTag: (tags: import("./CEconItem").Tag[], category: string) => import("./CEconItem").Tag | null;
     getImageURL: ({ icon_url }: import("./CEconItem").ItemDescription | import("./CEconItem").ItemDetails) => string;
     getLargeImageURL: ({ icon_url_large, icon_url }: import("./CEconItem").ItemDescription | import("./CEconItem").ItemDetails) => string;
