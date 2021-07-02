791c4f7aa2017bb108ea43924309a08dedfc1da6
Azul <diego.kfu@hotmail.com>
2021-07-02 00:07:25 -0300

    Cleanup

--- dist/CookieParser.d.ts
+++
@@ -1,4 +0,0 @@
-import { CookieJar } from 'tough-cookie';
-declare function CookieParser(RequestJar: any): Promise<CookieJar>;
-export default CookieParser;
-//# sourceMappingURL=CookieParser.d.ts.map
