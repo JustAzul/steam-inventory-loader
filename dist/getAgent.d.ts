7a14277dc038748d597d50ebd28daf7eb89e6350
Azul <diego.kfu@hotmail.com>
2022-07-03 11:43:25 -0300

    Cleanup

--- dist/getAgent.d.ts
+++
@@ -1,5 +0,0 @@
-import { HttpsAgent } from 'agentkeepalive';
-import { HttpsProxyAgent } from 'hpagent';
-declare function getAgent(proxyAddress: string): HttpsProxyAgent | HttpsAgent;
-export default getAgent;
-//# sourceMappingURL=getAgent.d.ts.map
