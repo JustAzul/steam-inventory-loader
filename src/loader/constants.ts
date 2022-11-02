c3d4c4deac64e3b6e4699fbb9f7334ffa0987f9a
Azul <metalloger@gmail.com>
2022-11-01 22:19:57 -0300

    changed default request delay

--- src/loader/constants.ts
+++ src/loader/constants.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_REQUEST_DELAY = 0;
+export const DEFAULT_REQUEST_DELAY = 1000 * 4; // 4 seconds
 export const DEFAULT_REQUEST_RESPONSE_LANGUAGE = 'english';
 export const DEFAULT_REQUEST_ITEMS_COUNT = 2000;
