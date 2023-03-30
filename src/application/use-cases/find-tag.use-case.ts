0ad7eb181e62c8f9738c194faf30c4bf8d5c7b4c
Azul <metalloger@gmail.com>
2023-03-30 09:26:08 -0300

    refactor(find-tag.use-case.ts): simplify FindTagUseCase.execute method and add type for its props.

--- src/application/use-cases/find-tag.use-case.ts
+++ src/application/use-cases/find-tag.use-case.ts
@@ -1,11 +1,19 @@
 import { rawTag } from '../../domain/types/raw-tag.type';
 
+export type FindTagUseCaseProps = {
+  tags: rawTag[];
+  categoryToFind: string;
+};
+
 export default class FindTagUseCase {
-  public static execute(
-    tags: Readonly<rawTag>[],
-    categoryToFind: string,
-  ): rawTag | null {
-    if (tags.length === 0) return null;
-    return tags.find(({ category }) => category === categoryToFind) ?? null;
+  public static execute(props: Readonly<FindTagUseCaseProps>): rawTag | null {
+    const hasAtLeastOneTag = Boolean(props?.tags?.length);
+    if (hasAtLeastOneTag === false) return null;
+
+    const tag: rawTag | undefined = props.tags.find(
+      ({ category }) => category === props.categoryToFind,
+    );
+
+    return tag ?? null;
   }
 }
