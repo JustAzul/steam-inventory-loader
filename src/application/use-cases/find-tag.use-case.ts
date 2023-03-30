3dde6291de07025b183a4af2b005909fca7c5f1a
Azul <metalloger@gmail.com>
2023-03-30 09:28:06 -0300

    refactor(find-tag.use-case.ts): change rawTag type to union with SteamItemTag feat(find-tag.use-case.ts): add support for SteamItemTag entity in FindTagUseCase

--- src/application/use-cases/find-tag.use-case.ts
+++ src/application/use-cases/find-tag.use-case.ts
@@ -1,16 +1,19 @@
+import SteamItemTag from '../../domain/entities/steam-item-tag.entity';
 import { rawTag } from '../../domain/types/raw-tag.type';
 
 export type FindTagUseCaseProps = {
-  tags: rawTag[];
+  tags: Array<rawTag | SteamItemTag>;
   categoryToFind: string;
 };
 
 export default class FindTagUseCase {
-  public static execute(props: Readonly<FindTagUseCaseProps>): rawTag | null {
+  public static execute(
+    props: Readonly<FindTagUseCaseProps>,
+  ): rawTag | SteamItemTag | null {
     const hasAtLeastOneTag = Boolean(props?.tags?.length);
     if (hasAtLeastOneTag === false) return null;
 
-    const tag: rawTag | undefined = props.tags.find(
+    const tag: rawTag | SteamItemTag | undefined = props.tags.find(
       ({ category }) => category === props.categoryToFind,
     );
 
