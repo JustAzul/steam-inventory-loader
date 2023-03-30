8a492ea318c4bdbb156ee8da9d557f8028f920c5
Azul <metalloger@gmail.com>
2023-03-29 21:04:09 -0300

    style(jest.config.ts): comment out notify property fix(jest.config.ts): change testEnvironment value to a string literal The notify property is now commented out, which removes the notifications for test results. The testEnvironment value is now a string literal, which improves consistency with the codebase.

--- jest.config.ts
+++ jest.config.ts
@@ -94,7 +94,7 @@ export default {
   // modulePathIgnorePatterns: [],
 
   // Activates notifications for test results
-  notify: true,
+  // notify: true,
 
   // An enum that specifies notification mode. Requires { notify: true }
   // notifyMode: "failure-change",
@@ -144,7 +144,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node"
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
