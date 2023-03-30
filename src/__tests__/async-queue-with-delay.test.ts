8d23f5bd95f0dadb9ff15d3cccb4744c3e5e13b1
Azul <metalloger@gmail.com>
2023-03-29 22:34:46 -0300

    test(async-queue-with-delay.test.ts): add test for error handling test(async-queue-with-delay.test.ts): refactor tests to use a factory function for creating AsyncQueueWithDelayEntity instances test(async-queue-with-delay.test.ts): add beforeEach to clear mocks and timers The tests now include a test for error handling, which ensures that the AsyncQueueWithDelayEntity instance throws a DomainException when the processItem function throws an error. The tests have been refactored to use a factory function for creating AsyncQueueWithDelayEntity instances, which allows for more flexibility in the tests. A beforeEach hook has been added to clear all mocks and timers before each test.

--- src/__tests__/async-queue-with-delay.test.ts
+++ src/__tests__/async-queue-with-delay.test.ts
@@ -2,31 +2,59 @@ import AsyncQueueWithDelayEntity, {
   AsyncQueueWithDelayProps,
 } from '../domain/entities/async-queue-with-delay.entity';
 
+import DomainException from '../domain/exceptions/domain.exception';
+
 jest.useFakeTimers();
 jest.spyOn(global, 'setTimeout');
 
 describe(AsyncQueueWithDelayEntity.name, () => {
   const delay = Number.MAX_SAFE_INTEGER;
 
-  const props: AsyncQueueWithDelayProps = {
+  const defaultProps: AsyncQueueWithDelayProps = {
     delayInMilliseconds: delay,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any
     processItem: jest.fn().mockImplementation((item: any) => item),
   };
 
-  const asyncQueueWithDelayEntity = new AsyncQueueWithDelayEntity(props);
-  const items: string[] = Array.from(new Array(10), (_, i) => String(i));
+  const asyncQueueWithDelayEntity = (
+    props: AsyncQueueWithDelayProps = defaultProps,
+  ) => new AsyncQueueWithDelayEntity(props);
 
-  beforeEach(() => jest.clearAllMocks());
+  const items: string[] = Array.from(new Array(3), (_, i) => String(i));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
+
+  it(`should return error instance`, async () => {
+    const props: AsyncQueueWithDelayProps = {
+      delayInMilliseconds: 1,
+      processItem: jest.fn().mockImplementation(() => {
+        throw new DomainException(AsyncQueueWithDelayEntity.name, 'test error');
+      }),
+    };
+
+    const asyncQueueWithDelayInstance = asyncQueueWithDelayEntity(props);
+
+    try {
+      await asyncQueueWithDelayInstance.insertAndProcess<string, string>(
+        'test item',
+      );
+    } catch (error) {
+      expect(error).toBeInstanceOf(DomainException);
+    }
+  });
 
   it(`should execute delays`, async () => {
+    const asyncQueueWithDelayInstance = asyncQueueWithDelayEntity();
     expect(setTimeout).not.toBeCalled();
 
     await Promise.all([
       jest.runAllTimersAsync(),
       Promise.all(
         items.map((item) =>
-          asyncQueueWithDelayEntity.insertAndProcess<string, string>(item),
+          asyncQueueWithDelayInstance.insertAndProcess<string, string>(item),
         ),
       ),
     ]);
@@ -35,25 +63,27 @@ describe(AsyncQueueWithDelayEntity.name, () => {
   });
 
   it(`should wait for a delay between calls`, async () => {
-    expect(props.processItem).not.toBeCalled();
+    expect(defaultProps.processItem).not.toBeCalled();
 
-    await asyncQueueWithDelayEntity.insertAndProcess('first call');
+    const asyncQueueWithDelayInstance = asyncQueueWithDelayEntity();
+    await asyncQueueWithDelayInstance.insertAndProcess('first call');
+
     jest.runAllTimers();
 
     expect(setTimeout).toBeCalled();
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), delay);
 
-    expect(props.processItem).toBeCalled();
-    expect(props.processItem).toHaveBeenCalledTimes(1);
+    expect(defaultProps.processItem).toBeCalled();
+    expect(defaultProps.processItem).toHaveBeenCalledTimes(1);
 
     await Promise.all([
-      asyncQueueWithDelayEntity.insertAndProcess('second call'),
+      asyncQueueWithDelayInstance.insertAndProcess('second call'),
       jest.runAllTimersAsync(),
     ]);
 
-    expect(props.processItem).toBeCalled();
-    expect(props.processItem).toHaveBeenCalledTimes(2);
+    expect(defaultProps.processItem).toBeCalled();
+    expect(defaultProps.processItem).toHaveBeenCalledTimes(2);
     expect(setTimeout).toBeCalled();
 
     expect(setTimeout).toHaveBeenCalledTimes(2);
@@ -61,11 +91,13 @@ describe(AsyncQueueWithDelayEntity.name, () => {
   });
 
   it(`should match results`, async () => {
+    const asyncQueueWithDelayInstance = asyncQueueWithDelayEntity();
+
     const [, results] = await Promise.all([
       jest.runAllTimersAsync(),
       Promise.all(
         items.map((item) =>
-          asyncQueueWithDelayEntity.insertAndProcess<string, string>(item),
+          asyncQueueWithDelayInstance.insertAndProcess<string, string>(item),
         ),
       ),
     ]);
