52dfdc081d7a840cdd7abeeebf685277ee6a8ecc
Azul <metalloger@gmail.com>
2023-03-30 09:22:39 -0300

    refactor(get-image-url.use-case.ts): simplify GetImageUrlUseCase.execute method and rename GetImageUrlUseCaseOptions to GetImageUrlUseCaseProps

--- src/application/use-cases/get-image-url.use-case.ts
+++ src/application/use-cases/get-image-url.use-case.ts
@@ -2,21 +2,21 @@ import { InputWithIconURL } from '../../domain/types/input-with-icon-url.type';
 /* eslint-disable camelcase */
 import { STEAM_CDN_IMAGE_URL } from '../../shared/constants';
 
-type GetImageUrlUseCaseOptions = {
-  size: 'normal' | 'large';
+type GetImageUrlUseCaseProps = {
+  input: InputWithIconURL;
+  size?: 'normal' | 'large';
 };
 
 export default class GetImageUrlUseCase {
-  public static execute(
-    input: InputWithIconURL,
-    option?: GetImageUrlUseCaseOptions,
-  ): string {
-    if (option?.size === 'large') {
+  public static execute(props: Readonly<GetImageUrlUseCaseProps>): string {
+    const isLarge = props?.size === 'large';
+
+    if (isLarge) {
       return `${STEAM_CDN_IMAGE_URL}/${
-        input?.icon_url_large || input.icon_url
+        props.input?.icon_url_large || props.input.icon_url
       }`;
     }
 
-    return `${STEAM_CDN_IMAGE_URL}/${input.icon_url}`;
+    return `${STEAM_CDN_IMAGE_URL}/${props.input.icon_url}`;
   }
 }
