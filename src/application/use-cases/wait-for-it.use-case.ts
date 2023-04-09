6f04bee3523e4ad0f6814c4784fee08773e059d4
Azul <metalloger@gmail.com>
2023-04-09 18:43:44 -0300

    docs(wait-for-it.use-case.ts): add JSDoc for execute method

--- src/application/use-cases/wait-for-it.use-case.ts
+++ src/application/use-cases/wait-for-it.use-case.ts
@@ -1,4 +1,9 @@
 export default class WaitForItUseCase {
+  /**
+   * Pauses execution for a specified amount of time.
+   *
+   * @param timeInMilliseconds - The amount of time to pause in milliseconds.
+   */
   public static async execute(timeInMilliseconds: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, timeInMilliseconds));
   }
