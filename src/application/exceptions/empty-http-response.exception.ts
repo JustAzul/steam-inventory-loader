f0ec2ded7afb23bf609b43c93123c1e3d4bb4bcc
Azul <metalloger@gmail.com>
2023-05-01 03:12:15 -0300

    refactor(empty-http-response.exception.ts): simplify EmptyHttpResponseExceptionProps type definition using Omit style(empty-http-response.exception.ts): change message to 'Empty HTTP Response' for better readability feat(empty-http-response.exception.ts): add request and response properties to EmptyHttpResponseException

--- src/application/exceptions/empty-http-response.exception.ts
+++ src/application/exceptions/empty-http-response.exception.ts
@@ -1,16 +1,13 @@
 import HttpException, { HttpExceptionProps } from './http.exception';
 
-type EmptyHttpResponseExceptionProps = Pick<
-  HttpExceptionProps,
-  'statusCode' | 'headers'
->;
+type EmptyHttpResponseExceptionProps = Omit<HttpExceptionProps, 'message'>;
 
 export default class EmptyHttpResponseException extends HttpException {
   public constructor(props: EmptyHttpResponseExceptionProps) {
     super({
-      headers: props.headers,
-      message: 'empty http response',
-      statusCode: props.statusCode,
+      message: 'Empty HTTP Response',
+      request: props.request,
+      response: props.response,
     });
   }
 }
