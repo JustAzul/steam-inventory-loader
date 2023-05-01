37f70c421710d25149ef79839f95d4834e428442
Azul <metalloger@gmail.com>
2023-05-01 03:09:50 -0300

    feat(http-client.spec.ts): add test for throwing HttpException on request failure refactor(http-client.spec.ts): update server route to include status code parameter for testing different status codes test(http-client.spec.ts): add test for sending request with correct headers

--- src/infra/__tests__/http-client.spec.ts
+++ src/infra/__tests__/http-client.spec.ts
@@ -1,25 +1,31 @@
 import Fastify from 'fastify';
 import FindOpenPort from 'find-open-port';
 import HttpClient from '../http-client';
+import HttpException from '../../application/exceptions/http.exception';
+import { IncomingHttpHeaders } from 'http';
+import StatusCode from 'status-code-enum';
+
+type ReceivedRequest = {
+  headers?: IncomingHttpHeaders;
+};
 
 describe(HttpClient.name, () => {
   const defaultServerResponse = { message: 'Hello World!' };
   const server = Fastify();
   let port = 0;
 
-  const receivedRequestData: Map<
-    string,
-    Record<'headers' | 'body', any>
-  > = new Map();
+  const receivedRequestData: Map<string, ReceivedRequest> = new Map();
 
   beforeAll(async () => {
-    server.get('/', ({ body, headers }, reply) => {
-      receivedRequestData.set('get', {
-        headers,
-        body,
+    server.get('/:status', (req, response) => {
+      const statusCodeToReply =
+        (req.params as Record<'status', number>)?.status || 200;
+
+      receivedRequestData.set(`get#${statusCodeToReply}`, {
+        headers: req.headers,
       });
 
-      reply.send(defaultServerResponse);
+      response.status(statusCodeToReply).send(defaultServerResponse);
     });
 
     port = await FindOpenPort();
@@ -32,6 +38,24 @@ describe(HttpClient.name, () => {
   afterAll(async () => server.close());
   beforeEach(() => receivedRequestData.clear());
 
+  it('should throw HTTP Exception when the request fails', async () => {
+    const httpClient = new HttpClient();
+
+    const forceStatusCodes = Object.keys(StatusCode)
+      .map(Number)
+      .filter((code) => Boolean(code) && (code < 200 || code >= 300));
+
+    for (const statusCode of forceStatusCodes) {
+      try {
+        await httpClient.get({
+          url: `http://localhost:${port}/${statusCode}`,
+        });
+      } catch (e) {
+        expect(e).toBeInstanceOf(HttpException);
+      }
+    }
+  });
+
   it('should send the request with the correct headers', async () => {
     const httpClient = new HttpClient();
 
@@ -39,14 +63,18 @@ describe(HttpClient.name, () => {
       'x-custom-header': 'custom-header-value',
     };
 
+    const forceStatusCode = StatusCode.SuccessOK;
+
     const httpClientResponse = await httpClient.get<
       typeof defaultServerResponse
     >({
-      url: `http://localhost:${port}`,
+      url: `http://localhost:${port}/${forceStatusCode}`,
       headers,
     });
 
-    const httpServerResponse = receivedRequestData.get('get');
+    const httpServerResponse = receivedRequestData.get(
+      `get#${forceStatusCode}`,
+    );
 
     const hasHttpServerResponse = Boolean(httpServerResponse);
     const hasHttpClientResponse = Boolean(httpClientResponse);
