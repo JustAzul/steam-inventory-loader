1eae7429070686969720b252840ee502663a3261
Azul <metalloger@gmail.com>
2023-05-01 03:10:25 -0300

    style(validate-endpoint.spec.ts): reformat code for better readability and consistency
    No functional changes were made, only code formatting adjustments to improve readability and maintain consistency across the project.

--- src/application/use-cases/__tests__/validate-endpoint.spec.ts
+++ src/application/use-cases/__tests__/validate-endpoint.spec.ts
@@ -1,61 +1,70 @@
 import {
-    PLACEHOLDER_APP_ID,
-    PLACEHOLDER_CONTEXT_ID,
-    PLACEHOLDER_STEAM_ID_64,
+  PLACEHOLDER_APP_ID,
+  PLACEHOLDER_CONTEXT_ID,
+  PLACEHOLDER_STEAM_ID_64,
 } from '../../../shared/constants';
 
 import UseCaseException from '../../exceptions/use-case.exception';
 import ValidateEndpointUseCase from '../validate-endpoint.use-case';
 
 describe(ValidateEndpointUseCase.name, () => {
-    const validEndpoint = `https://example.com/${PLACEHOLDER_APP_ID}/${PLACEHOLDER_CONTEXT_ID}/${PLACEHOLDER_STEAM_ID_64}`;
-  
-    it('should not throw any exception for a valid endpoint', () => {
-      const validateEndpointUseCase = new ValidateEndpointUseCase({ endpoint: validEndpoint });
-  
-      expect(() => {
-        validateEndpointUseCase.execute();
-      }).not.toThrow();
+  const validEndpoint = `https://example.com/${PLACEHOLDER_APP_ID}/${PLACEHOLDER_CONTEXT_ID}/${PLACEHOLDER_STEAM_ID_64}`;
+
+  it('should not throw any exception for a valid endpoint', () => {
+    const validateEndpointUseCase = new ValidateEndpointUseCase({
+      endpoint: validEndpoint,
     });
-  
-    it('should throw UseCaseException for missing protocol', () => {
-      const invalidEndpoint = validEndpoint.replace('https://', '');
-  
-      const validateEndpointUseCase = new ValidateEndpointUseCase({ endpoint: invalidEndpoint });
-  
-      expect(() => {
-        validateEndpointUseCase.execute();
-      }).toThrow(UseCaseException);
-    });
-  
-    it('should throw UseCaseException for missing PLACEHOLDER_STEAM_ID_64', () => {
-      const invalidEndpoint = validEndpoint.replace(PLACEHOLDER_STEAM_ID_64, '');
-  
-      const validateEndpointUseCase = new ValidateEndpointUseCase({ endpoint: invalidEndpoint });
-  
-      expect(() => {
-        validateEndpointUseCase.execute();
-      }).toThrow(UseCaseException);
-    });
-  
-    it('should throw UseCaseException for missing PLACEHOLDER_APP_ID', () => {
-      const invalidEndpoint = validEndpoint.replace(PLACEHOLDER_APP_ID, '');
-  
-      const validateEndpointUseCase = new ValidateEndpointUseCase({ endpoint: invalidEndpoint });
-  
-      expect(() => {
-        validateEndpointUseCase.execute();
-      }).toThrow(UseCaseException);
-    });
-  
-    it('should throw UseCaseException for missing PLACEHOLDER_CONTEXT_ID', () => {
-      const invalidEndpoint = validEndpoint.replace(PLACEHOLDER_CONTEXT_ID, '');
-  
-      const validateEndpointUseCase = new ValidateEndpointUseCase({ endpoint: invalidEndpoint });
-  
-      expect(() => {
-        validateEndpointUseCase.execute();
-      }).toThrow(UseCaseException);
-    });
+
+    expect(() => {
+      validateEndpointUseCase.execute();
+    }).not.toThrow();
   });
-  
+
+  it('should throw UseCaseException for missing protocol', () => {
+    const invalidEndpoint = validEndpoint.replace('https://', '');
+
+    const validateEndpointUseCase = new ValidateEndpointUseCase({
+      endpoint: invalidEndpoint,
+    });
+
+    expect(() => {
+      validateEndpointUseCase.execute();
+    }).toThrow(UseCaseException);
+  });
+
+  it('should throw UseCaseException for missing PLACEHOLDER_STEAM_ID_64', () => {
+    const invalidEndpoint = validEndpoint.replace(PLACEHOLDER_STEAM_ID_64, '');
+
+    const validateEndpointUseCase = new ValidateEndpointUseCase({
+      endpoint: invalidEndpoint,
+    });
+
+    expect(() => {
+      validateEndpointUseCase.execute();
+    }).toThrow(UseCaseException);
+  });
+
+  it('should throw UseCaseException for missing PLACEHOLDER_APP_ID', () => {
+    const invalidEndpoint = validEndpoint.replace(PLACEHOLDER_APP_ID, '');
+
+    const validateEndpointUseCase = new ValidateEndpointUseCase({
+      endpoint: invalidEndpoint,
+    });
+
+    expect(() => {
+      validateEndpointUseCase.execute();
+    }).toThrow(UseCaseException);
+  });
+
+  it('should throw UseCaseException for missing PLACEHOLDER_CONTEXT_ID', () => {
+    const invalidEndpoint = validEndpoint.replace(PLACEHOLDER_CONTEXT_ID, '');
+
+    const validateEndpointUseCase = new ValidateEndpointUseCase({
+      endpoint: invalidEndpoint,
+    });
+
+    expect(() => {
+      validateEndpointUseCase.execute();
+    }).toThrow(UseCaseException);
+  });
+});
