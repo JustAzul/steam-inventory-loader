ba60d62c380c7e18be946d072644547ccb9b9e11
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-02-04 17:59:58 -0300

    🔧 refactor(.eslintrc.js): update ESLint rules and plugins for better code quality control 🔧 refactor(package.json): update dependencies to their latest versions for improved functionality and security 🔧 feat(package.json): add new ESLint plugins for sorting keys and TypeScript sorting keys for better code organization

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,57 @@
 module.exports = {
-    env: {
-      es2021: true,
-      node: true,
-    },
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly"
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:prettier/recommended',
-    ],
-    plugins: ['@typescript-eslint', 'import'],
-    parser: '@typescript-eslint/parser',
-    rules: {
-      'import/extensions': [
-        'error',
-        'ignorePackages',
-        {
-          js: 'never',
-          ts: 'never',
-        },
-      ],
-      'import/order': 'off',
-      'import/prefer-default-export': 'warn',
-    },
-    overrides: [
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'sort-keys',
+    'typescript-sort-keys',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:typescript-sort-keys/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'import/order': [
+      'error',
       {
-        files: ['*.ts', '*.tsx'],
-        extends: [
-          'plugin:@typescript-eslint/recommended',
-          'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        ],
-        parserOptions: {
-          project: ['./tsconfig.json'],
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
         },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
       },
     ],
-  };
-  
+    'sort-keys': 0,
+    'sort-keys/sort-keys-fix': 1,
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'warn',
+  },
+};
