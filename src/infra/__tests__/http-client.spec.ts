dcdb2d85a0971d740221117e2883dafde5566fa0
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-02-24 15:11:45 -0300

    ✨ feat(http-client.spec.ts): add test for checking correct headers in request

--- src/infra/__tests__/http-client.spec.ts
+++ src/infra/__tests__/http-client.spec.ts
@@ -1,3 +1,4 @@
+import { UUID, randomUUID } from 'crypto';
 import { IncomingHttpHeaders } from 'http';
 
 import { IHttpClient } from '@application/ports/http-client.interface';
@@ -6,8 +7,9 @@ import Fastify from 'fastify';
 
 const ERROR_STATUS_CODES = [401, 402, 403, 404, 500];
 
-type ReceivedRequest = {
-  headers?: IncomingHttpHeaders;
+type RequestData = {
+  // body?: string;
+  headers: IncomingHttpHeaders;
 };
 
 executeTest(new HttpClient());
@@ -18,25 +20,66 @@ function executeTest(httpClient: IHttpClient) {
     const server = Fastify();
     let address: string;
 
-    const receivedRequestData: Map<string, ReceivedRequest> = new Map();
+    const requestsMap: Map<UUID, RequestData> = new Map();
 
     beforeAll(async () => {
-      server.get('/:status', (req, response) => {
-        const statusCodeToReply =
+      server.get('/:status', (req, res) => {
+        const targetStatusCode =
           (req.params as Record<'status', number>)?.status || 200;
 
-        receivedRequestData.set(`get#${statusCodeToReply}`, {
-          headers: req.headers,
+        const requestId = randomUUID();
+        const headersReceived = req.headers;
+
+        requestsMap.set(requestId, {
+          // body: JSON.stringify(req.body),
+          headers: headersReceived,
         });
 
-        response.status(statusCodeToReply).send(defaultServerResponse);
+        res
+          .status(targetStatusCode)
+          .header('x-request-id', requestId)
+          .send(defaultServerResponse);
       });
 
       address = await server.listen();
     });
 
     afterAll(async () => server.close());
-    beforeEach(() => receivedRequestData.clear());
+    beforeEach(() => requestsMap.clear());
+
+    it('should send the request with the correct headers', async () => {
+      const headers = {
+        'x-custom-header': 'custom-header-value',
+      };
+
+      const forceStatusCode = 200;
+
+      const [error, response] = await httpClient.get<
+        typeof defaultServerResponse
+      >({
+        headers,
+        url: `${address}/${forceStatusCode}`,
+      });
+
+      expect(error).toBeUndefined();
+      expect(response?.headers['x-request-id']).toBeDefined();
+
+      const httpServerResponse = requestsMap.get(
+        response?.headers['x-request-id'] as UUID,
+      );
+
+      expect(httpServerResponse).toBeDefined();
+      expect(httpServerResponse?.headers).toMatchObject(headers);
+    });
+
+    it('should complete a GET request', async () => {
+      const [error, response] = await httpClient.get({
+        url: `${address}/200`,
+      });
+
+      expect(error).toBeUndefined();
+      expect(response?.data).toMatchObject(defaultServerResponse);
+    });
 
     it('should not return response when request fails', async () => {
       const results = await Promise.all(
